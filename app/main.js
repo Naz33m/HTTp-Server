19e07baa6231ca8a436889972f8dc59cf4e91d37
Naz33m <mohammednazeemnoushad@gmail.com>
2025-04-10 13:59:05 +0400

    Respond with 200 or 400

--- app/main.js
+++ app/main.js
@@ -5,8 +5,20 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", () => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    const request = data.toString();
+    const lines = request.split("\r\n");
+    const firstLine = lines[0];
+    const parts = firstLine.split(" ");
+    const path = parts[1];
+    console.log(firstLine);
+    console.log(parts);
+    console.log(path);
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
     socket.end();
   });
   // socket.write("HTTP/1.1 200 OK \r\n\r\n");
