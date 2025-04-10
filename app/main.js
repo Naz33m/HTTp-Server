203257b278290c7d5ac37f51384cb9a5a0d1e61c
Naz33m <mohammednazeemnoushad@gmail.com>
2025-04-10 14:32:43 +0400

    response last string from the url path

--- app/main.js
+++ app/main.js
@@ -14,14 +14,21 @@ const server = net.createServer((socket) => {
     let responseBody = "";
     if (path.startsWith("/echo/")) {
       responseBody = path.substring("/echo/".length);
-      console.log(responseBody);
+      const contentLength = responseBody.length;
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: ${contentLength}\r\n\r\n${responseBody}`,
+      );
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      socket.destroy();
     }
-    const contentLength = Buffer.byteLength(responseBody);
-    socket.write("HTTP/1.1 200 OK\r\n");
-    socket.write("Content-Type: text/plain\r\n");
-    socket.write(`Content-Length: ${contentLength}\r\n`);
-    socket.write("\r\n");
-    socket.write(responseBody);
+  });
+
+  socket.on("error", (e) => {
+    console.error("ERROR: ", e);
+    socket.end();
+  });
+  socket.on("close", () => {
     socket.end();
   });
 });
