5ab7b60685adb76ac1ee2d8af1901219f75dfdd8
Naz33m <mohammednazeemnoushad@gmail.com>
2025-04-11 09:19:51 +0400

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -17,9 +17,30 @@ const server = net.createServer((socket) => {
       socket.write("HTTP/1.1 200 OK\r\nContent-length: 0\r\n\r\n");
     } else if (path.startsWith("/echo/")) {
       responseBody = path.substring("/echo/".length);
+      console.log(lines[4]);
+      let acceptEncoding;
+      for (let i = 1; i < lines.length; i++) {
+        if (lines[i].toLowerCase().startsWith("accept-encoding:")) {
+          const encodings = lines[i]
+            .split(":")[1]
+            .trim()
+            .split(",")
+            .map((e) => e.trim());
+          if (encodings.includes("gzip")) {
+            acceptEncoding = "gzip";
+          } else if (encodings.includes("deflate")) {
+            acceptEncoding = "deflate";
+          } else if (encodings.includes("br")) {
+            acceptEncoding = "br";
+          } else {
+            acceptEncoding = "";
+          }
+          break;
+        }
+      }
       const contentLength = responseBody.length;
       socket.write(
-        `HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: ${contentLength}\r\n\r\n${responseBody}`,
+        `HTTP/1.1 200 OK\r\nContent-Encoding:${acceptEncoding && acceptEncoding}\r\nContent-type: text/plain\r\nContent-length: ${contentLength}\r\n\r\n${responseBody}`,
       );
     } else if (path.startsWith("/user-agent")) {
       const headers = {};
