5b0903a2c483249e0523fade10f1d24145e5dc93
Naz33m <mohammednazeemnoushad@gmail.com>
2025-04-23 16:36:40 +0400

    Connection close

--- app/main.js
+++ app/main.js
@@ -3,9 +3,10 @@ const fs = require("fs");
 const zlib = require("zlib");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  let close = 0;
+
   socket.on("data", (data) => {
     const request = data.toString();
     const lines = request.split("\r\n");
@@ -13,16 +14,24 @@ const server = net.createServer((socket) => {
     const parts = firstLine.split(" ");
     const path = parts[1];
     let responseBody = "";
+    let indexOfHeaderClose = lines.filter(
+      (x, index) => x.includes("Connection") && index,
+    );
+    close = indexOfHeaderClose.length > 0 ? 1 : 0;
     if (path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\nContent-length: 0\r\n\r\n");
+      console.log("inside the base path ");
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-length: 0\r\n${lines[indexOfHeaderClose]} \r\n`,
+      );
+      if (close === 1) {
+        socket.end();
+      }
     } else if (path.startsWith("/echo/")) {
       let responseBody = path.substring("/echo/".length);
       let acceptEncoding = null;
-
       for (let i = 1; i < lines.length; i++) {
         const line = lines[i];
         if (line === "") break;
-
         if (line.toLowerCase().startsWith("accept-encoding:")) {
           const encodings = line
             .split(":")[1]
@@ -40,6 +49,8 @@ const server = net.createServer((socket) => {
           break;
         }
       }
+      // socket.write will send data to the client but will not close the connection
+      // socket.end() or socket.close() are commonly used for closing connections
       socket.write("HTTP/1.1 200 OK\r\n");
       socket.write("Content-Type: text/plain\r\n");
       if (acceptEncoding === "gzip") {
@@ -65,9 +76,28 @@ const server = net.createServer((socket) => {
       }
       const userAgent = headers["user-agent"];
       const contentLength = userAgent ? userAgent.length : 0;
-      socket.write(
-        `HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: ${contentLength}\r\n\r\n${userAgent || ""}`,
-      );
+      // socket.write(
+      //   `HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: ${contentLength}\r\nUser-Agent: ${userAgent || ""}\r\nConnection:${connectionClose || ""}`,
+      // );
+      if (headers["connection"]) {
+        console.log("connection header found");
+        socket.write("HTTP/1.1 200 OK\r\n");
+        socket.write("Content-Type: text/plain\r\n");
+        socket.write(`Content-Length: ${contentLength}\r\n`);
+        socket.write(`Connection: ${headers["connection"]}\r\n`);
+        if (headers["user-agent"]) {
+          socket.write(userAgent);
+        }
+        socket.end();
+      } else {
+        console.log("no connection header found");
+        socket.write("HTTP/1.1 200 OK\r\n");
+        socket.write("Content-Type: text/plain\r\n");
+        socket.write(`Content-Length: ${contentLength}\r\n`);
+        if (headers["user-agent"]) {
+          socket.write(`User-Agent: ${userAgent}\r\n`);
+        }
+      }
     } else if (path.startsWith("/files/")) {
       const filename = path.substring("/files/".length);
       const directory = process.argv[3];
@@ -111,3 +141,7 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(4221, "localhost");
+
+// curl --http1.1 -v
+//   http://localhost:4221/echo/blueberry
+// --next http://localhost:4221/echo/pear -H "Connection: close"
